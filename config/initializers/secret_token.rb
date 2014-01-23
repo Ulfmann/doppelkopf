59976f92cb787f54be4b6cab34a9f9b7abdf0be8
Ulfmann <ulf.portner@googlemail.com>
2014-01-23 17:13:25 +0100

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doppelkopf::Application.config.secret_key_base = '69cef635b7bf333912cce16949d8a3e930797e0ec77667d393006b58397f141bffaef378174b54226b60c5e710a0cd260ea76aa700807fc82404c3397976f7e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Doppelkopf::Application.config.session_store :cookie_store, key: '_doppelkopf_session'
